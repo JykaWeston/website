092b988ab3aa837ec0166f712a2376e54c33a4f2
jykaweston <dylandelbauve@gmail.com>
2020-08-06 17:50:51 +0200

    Add mentions légales

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("header").style.fontSize = "30px";
+      document.getElementById("header").style.fontSize = "20px";
+      document.getElementById("navbar").style.height = "5px";
     } else {
-      document.getElementById("header").style.fontSize = "90px";
+      document.getElementById("header").style.fontSize = "30px";
     }
   } 
